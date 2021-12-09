dc432e84139edab2f105d86151a0d9586c72ebd8
big-star <88776942+ryosha5140@users.noreply.github.com>
2021-12-09 20:45:12 +1000

    -fix some issues

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 // If you want to start measuring performance in your app, pass a function
